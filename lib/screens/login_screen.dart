4d77bbd43e6607976135471a2a2a8073ea129f75
Aalem Daneshyar <aalem.daneshyar87@gmail.com>
2022-12-19 11:05:38 +0430

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,7 +2,6 @@ import '/constants.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  static const String id = 'login_screen';
 
   @override
   _LoginScreenState createState() => _LoginScreenState();
